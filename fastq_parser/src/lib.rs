55b63bd7aae8d5232db9a14b092882277dee3b1d
g610g <gio.gonzales@carsu.edu.ph>
2025-02-12 08:10:42 +0000

    refactor parser

--- fastq_parser/src/lib.rs
+++ fastq_parser/src/lib.rs
@@ -1,4 +1,5 @@
 use bio::io::fastq;
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -6,20 +7,35 @@ use pyo3::prelude::*;
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
+
+//creates python bindings that will be used for parsing fastq files
 #[pyfunction]
 fn parse_fastq_file(file_path: String) -> PyResult<Vec<String>> {
-    let mut read_vector = vec![];
-    let mut reader = fastq::Reader::from_file(file_path).unwrap();
-    let mut nb_read = 0;
-    let mut nb_bases = 0;
-    for result in reader.records() {
-        let record = result.unwrap();
-        let string_record = std::str::from_utf8(record.seq()).unwrap().to_string();
-        read_vector.push(string_record);
+    match fastq::Reader::from_file(file_path) {
+        Ok(reader) => {
+            let mut read_vector = vec![];
+            for result in reader.records() {
+                if let Ok(record) = result {
+                    let string_record = byte_to_string(record.seq());
+                    read_vector.push(string_record);
+                }
+            }
+            return Ok(read_vector);
+        }
+        Err(e) => {
+            //code panics for now. Will add better error handling
+            panic!("Something went wrong during finding the file")
+        }
     }
 
-    Ok(read_vector)
+    Err(PyErr::new::<PyTypeError, _>("Something went wrong"))
 }
+
+//this function unwraps for now. Add better error handling
+fn byte_to_string(byte_array: &[u8]) -> String {
+    std::str::from_utf8(byte_array).unwrap().to_string()
+}
+
 #[pymodule]
 fn fastq_parser(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
